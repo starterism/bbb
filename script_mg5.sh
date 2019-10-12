0021943661e0d624edd275b74e05f69f407a592f
wxh <wangxiaohu@danshen.im>
2019-10-12 15:32:45 +0800

    change

--- script_mg5.sh
+++ script_mg5.sh
@@ -1,29 +1,9 @@
 #! /bin/bash
 
-mg5path=/home/gangli/Documents/Simulation/MG5_aMC_v2_3_3/bin/
-txtpath=/home/gangli/Documents/Simulation/MG5_aMC_v2_3_3/bin/TOPEFT_Project/decay/mg5_txt
+mg5path=~/MG/bin/
+txtpath=~/bbb/mg5_txt
 
-# cp $txtpath/mg5_topdecay_SM.txt $mg5path
-# cd $mg5path
-# ./mg5_aMC mg5_topdecay_SM.txt
-# rm -f $mg5path/mg5_topdecay_SM.txt
-#
-# cp $txtpath/mg5_topdecay_CtW_int.txt $mg5path
-# cd $mg5path
-# ./mg5_aMC mg5_topdecay_CtW_int.txt
-# rm -f $mg5path/mg5_topdecay_CtW_int.txt
-#
-# cp $txtpath/mg5_topdecay_CtW_sq.txt $mg5path
-# cd $mg5path
-# ./mg5_aMC mg5_topdecay_CtW_sq.txt
-# rm -f $mg5path/mg5_topdecay_CtW_sq.txt
-#
-cp $txtpath/mg5_topdecay_Cqq_int.txt $mg5path
+cp $txtpath/mg5_bbb_bk_bbb.txt $mg5path
 cd $mg5path
-./mg5_aMC mg5_topdecay_Cqq_int.txt
-rm -f $mg5path/mg5_topdecay_Cqq_int.txt
-#
-# cp $txtpath/mg5_topdecay_Cqq_sq.txt $mg5path
-# cd $mg5path
-# ./mg5_aMC mg5_topdecay_Cqq_sq.txt
-# rm -f $mg5path/mg5_topdecay_Cqq_sq.txt
+./mg5_aMC mg5_bbb_bk_bbb.txt
+rm -f $mg5path/mg5_bbb_bk_bbb.txt
