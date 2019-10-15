66a8f935db61412372ced8c08057e7df7243ebb0
wxh <wangxiaohu@danshen.im>
2019-10-16 00:29:54 +0800

    add

--- script_mg5_sg.sh
+++ script_mg5_sg.sh
@@ -9,17 +9,17 @@ for MSIX2 in 200 400 600 800 1000
 
 do
 
-	cp $txtpath/mg5_bbb_sg_$MSIX21.txt $mg5path
-	cp $txtpath/mg5_bbb_sg_$MSIX22.txt $mg5path
+	cp $txtpath/mg5_bbb_sg_${MSIX2}_1.txt $mg5path
+	cp $txtpath/mg5_bbb_sg_${MSIX2}_2.txt $mg5path
 	cd $mg5path
-	./mg5_aMC mg5_bbb_sg_$MSIX21.txt
-	rm -f $mg5path/mg5_bbb_sg_$MSIX21.txt
+	./mg5_aMC mg5_bbb_sg_${MSIX2}_1.txt
+	rm -f $mg5path/mg5_bbb_sg_${MSIX2}_1.txt
 
-	\cp -rf $toolpath/run_delphes3 $outputpath/$MSIX2/bin/internal
+	\cp -rf $toolpath/run_delphes3 $outputpath/${MSIX2}/bin/internal
 	cd $mg5path
-	./mg5_aMC mg5_bbb_sg_$MSIX22.txt
-	rm -f $mg5path/mg5_bbb_sg_$MSIX22.txt
-	cd $outputpath/$MSIX2/Events
+	./mg5_aMC mg5_bbb_sg_${MSIX2}_2.txt
+	rm -f $mg5path/mg5_bbb_sg_${MSIX2}_2.txt
+	cd $outputpath/${MSIX2}/Events
 	find . -name "*.root" -exec rm -rf {} \;
 	find . -name "*.hepmc.gz" -exec rm -rf {} \;
 
