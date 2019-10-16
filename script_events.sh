aafaeef0150aaa8d9bd8cd8fd4fa8605fd259e0a
wxh <wangxiaohu@danshen.im>
2019-10-16 17:32:39 +0800

    add

--- script_events.sh
+++ script_events.sh
@@ -9,19 +9,21 @@ do
 	if [ ${b_s} == background ]; then
 		for final_state in bbb bbj bjj jjj
 		do
+			mkdir -p ${topath}/${b_s}/${final_state}
 			${i} = 0
 			while [[ ${i} -le 9 ]]; do
 				\cp ${frompath}/${b_s}/${final_state}/Events/run_01_${i}/tag_1_delphes_events.lhco.gz ${topath}/${b_s}/${final_state}/${i}.lhco.gz
-				((${i}++))
+				((i++))
 			done
 		done	
 	else
 		for final_state in 200 400 600 800 1000
 		do
+			mkdir -p ${topath}/${b_s}/${final_state}
 			${i} = 0
 			while [[ ${i} -le 9 ]]; do
 				\cp ${frompath}/${b_s}/${final_state}/Events/run_01_${i}/tag_1_delphes_events.lhco.gz ${topath}/${b_s}/${final_state}/${i}.lhco.gz
-				((${i}++))
+				((i++))
 			done
 		done	
 	fi	
