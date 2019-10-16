b91c696e29e6f64cdc44affbee596394cdcba947
wxh <wangxiaohu@danshen.im>
2019-10-16 17:38:06 +0800

    add

--- script_events.sh
+++ script_events.sh
@@ -2,7 +2,6 @@
 
 frompath=~/MG5_aMC_v2_6_6/bin/bbb
 topath=~/bbb
-i=0
 
 for b_s in background signal
 do
@@ -10,7 +9,7 @@ do
 		for final_state in bbb bbj bjj jjj
 		do
 			mkdir -p ${topath}/${b_s}/${final_state}
-			${i} = 0
+			i=0
 			while [[ ${i} -le 9 ]]; do
 				\cp ${frompath}/${b_s}/${final_state}/Events/run_01_${i}/tag_1_delphes_events.lhco.gz ${topath}/${b_s}/${final_state}/${i}.lhco.gz
 				((i++))
@@ -20,7 +19,7 @@ do
 		for final_state in 200 400 600 800 1000
 		do
 			mkdir -p ${topath}/${b_s}/${final_state}
-			${i} = 0
+			i=0
 			while [[ ${i} -le 9 ]]; do
 				\cp ${frompath}/${b_s}/${final_state}/Events/run_01_${i}/tag_1_delphes_events.lhco.gz ${topath}/${b_s}/${final_state}/${i}.lhco.gz
 				((i++))
